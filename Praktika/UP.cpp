16923f3f57b53b95d83f477031fe8d0ee8ee088c
Alexander Petrushin <110337515+Hugglet@users.noreply.github.com>
2023-08-15 12:52:07 +0300

    Update UP.cpp
    Сменил русский язык на английский в коде

--- Praktika/UP.cpp
+++ Praktika/UP.cpp
@@ -2,10 +2,10 @@
 #include <locale.h>
 
 using namespace std;
-const int RAZ = 100;		        //ìàêñèìàëüíûé ðàçìåð ìàòðèöû
-typedef int telem;		            //îïðåäåëåíèå òèïà çíà÷åíèé ýëåìåíòîâ ìàññèâà
-typedef telem tmatr[RAZ][RAZ];	    //îïðåäåëåíèå òèïà äâóìåðíîãî ìàññèâà
-typedef telem tmas[RAZ * RAZ];	    //îïðåäåëåíèå òèïà îäíîìåðíîãî ìàññèâà
+const int RAZ = 100;		        //maximum matrix size
+typedef int telem;		            //determining the type of values of array elements
+typedef telem tmatr[RAZ][RAZ];	    //determining the type of a two-dimensional array
+typedef telem tmas[RAZ * RAZ];	    //defining the type of a one-dimensional array
 
 void input_matr(tmatr a, int& n);
 void output_matr(tmatr a, int n);
@@ -15,30 +15,28 @@ void obnul(tmatr&, int);
 
 int main()
 {
-    system("chcp 1251");
-    setlocale(LC_ALL, "Rus");
-    int n;                          //ðàçìåð êâàäðàòíîé ìàòðèöû (2n)*(2n)
-    tmatr a;                        //èñõîäíàÿ ìàòðèöà (2n)*(2n)
-    input_matr(a, n);               //ââîä çíà÷åíèé è ðàçìåðà ìàòðèöû (2n)*(2n)
-    cout << "Èñõîäíàÿ ìàòðèöà: \n";
-    output_matr(a, n);              //âûâîä èñõîäíîé ìàòðèöû (2n)*(2n)
+    int n;                          //square matrix size (2n)*(2n)
+    tmatr a;                        //the original matrix is (2n)*(2n)
+    input_matr(a, n);               //entering values and matrix size (2n)*(2n)
+    cout << "The original matrix: \n";
+    output_matr(a, n);              //output of the original matrix (2n)*(2n)
     sort_matr(a, n);
     obnul(a, n);
-    cout << "Ìàòðèöà ïîñëå ñîðòèðîâêè: \n";
-    output_matr(a, n);              //âûâîä ïîëó÷åííîãî ìàññèâà
+    cout << "Matrix after sorting: \n";
+    output_matr(a, n);              //output of the received array
     cout << "\n";
     return 0;
 }
 
 //input_matr_beg_____________________________________________________________
-//Ââîä èñõîäíûõ äàííûõ
+//Input of initial data
 void input_matr(tmatr a, int& n)
 {
-    cout << "Ââåäèòå ÷èñëî n - ðàçìåð êâàäðàòíîé ìàòðèöû (2n)*(2n): ";
+    cout << "Enter the number n - the size of the square matrix (2n)*(2n): ";
     cin >> n;
-    cout << "Ââåäèòå ïîñòðî÷íî ÷åðåç ïðîáåë ýëåìåíòû ";
-    cout << "ìàòðèöû ðàçìåðà " << 2 * n << "x" << 2 * n << endl;
-    cout << "Ïîñëå ââîäà ñòðîêè íàæèìàéòå <Enter>" << endl;
+    cout << "Enter the elements line by line separated by a space ";
+    cout << "size matrices " << 2 * n << "x" << 2 * n << endl;
+    cout << "After entering the line, press <Enter>" << endl;
     for (int i = 0; i < 2 * n; i++)
     {
         for (int j = 0; j < 2 * n; j++)
@@ -51,7 +49,7 @@ void input_matr(tmatr a, int& n)
 //input_matr_end_____________________________________________________________
 
 //output_matr_beg____________________________________________________________
-//Âûâîä ìàòðèöû
+//Matrix output
 void output_matr(tmatr a, int n)
 {
     cout << endl;
@@ -70,19 +68,19 @@ void output_matr(tmatr a, int n)
 //output_matr_end____________________________________________________________
 
 //sort_vibor_max_beg_________________________________________________________
-//Ñîðòèðîâêà ýëåìåíòîâ ìàññèâà ïî óáûâàíèþ ìåòîäîì âûáîðà ìàêñèìóìà
+//Sorting array elements in descending order by the maximum selection method
 void sort_vibor_max(tmas& a, int n)
 {
     int i, k, imax;
     telem z;
     for (i = 0; i < n - 1; i++)
     {
-        imax = i;		//ïîèñê î÷åðåäíîãî ìàêñèìóìà
+        imax = i;		//search for the next maximum
         for (k = n; k > i; k--)
         {
             if (a[k] > a[imax]) imax = k;
         }
-        //ïåðåñòàíîâêà ýëåìåíòîâ
+        //rearranging elements
         z = a[i];
         a[i] = a[imax];
         a[imax] = z;
@@ -95,7 +93,7 @@ void sort_matr(tmatr& a, int n)
 {
     int c = 0;
     tmas ar;
-    for (int j = 0; j < n; j++)          //ëåâàÿ ñîðòèðóåìàÿ ÷àñòü ìàòðèöû
+    for (int j = 0; j < n; j++)          //the left sortable part of the matrix
     {
         for (int i = j; i < 2 * n - j; i++)
         {
@@ -112,7 +110,7 @@ void sort_matr(tmatr& a, int n)
         }
         c = 0;
     }
-    for (int j = n; j < 2 * n; j++)     //ïðàâàÿ ñîðòèðóåìàÿ ÷àñòü ìàòðèöû
+    for (int j = n; j < 2 * n; j++)     //the right sortable part of the matrix
     {
         c = 0;
         for (int i = 2 * n - j - 1; i < j + 1; i++)
